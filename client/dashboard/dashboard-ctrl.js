50d717a8174b2a017b08b7c15c4241cb539c9e48
nbiton <naor.biton@gmail.com>
2015-10-11 01:44:34 +0800

    Added the device model + simple example on how to use it in the dashboard

--- client/dashboard/dashboard-ctrl.js
+++ client/dashboard/dashboard-ctrl.js
@@ -2,16 +2,13 @@
  * Created by naor on 10/10/15.
  */
 angular.module('IoThingies.dashboard').controller('DashboardCtrl', [
+  '$meteor',
+  function ($meteor) {
+    const vm = this;
 
-  function () {
-
-    var vm = this;
-
-
+    vm.devices = $meteor.collection(Devices);
     vm.counter = 0;
 
-    vm.increase = function () {
-      vm.counter++;
-    };
+    vm.increase = ()=> vm.counter++;
   }
 ]);
---
+++ models/devices-collection.js
@@ -0,0 +1,4 @@
+/**
+ * Created by naor on 10/11/15.
+ */
+Devices = new Mongo.Collection('devices');
